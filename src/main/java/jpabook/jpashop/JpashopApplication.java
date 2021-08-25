af0084cdc2b65aa5c2a9d76d9759ee7c13b54366
SunChanKwon <ksc7662@naver.com>
2021-08-25 17:55:02 +0900

    Study 제이쿼리

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -12,7 +12,7 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 
 		Hello hello = new Hello();
-		hello.setData("hello");
+		hello.setData("이유나 바보 입니다.");
 		String data = hello.getData();
 		System.out.println("data = "+ data); //lOMBOOKTest
 
