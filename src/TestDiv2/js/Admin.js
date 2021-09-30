ddfd61d36dc3e4947dca46564728f35e6442477a
D-Kwon <ksc7662@naver.com>
2021-09-30 20:51:44 +0900

    commit front

---
+++ src/TestDiv2/js/Admin.js
@@ -0,0 +1,26 @@
+﻿$('#loginBtn').click(function () {
+    $('.loginModal').css('display', 'block');
+});
+
+$(()=>{
+    $('#Meunselect li').click(function(){
+        var idx = $(this).index();
+        $('#mypageTab li').removeClass('on');
+        $('#mypageTab li').eq(idx).addClass('on');
+        $('#mypageTab>div').hide();
+        $('#mypageTab>div').eq(idx).show();
+    });
+});
+/*test*/
+function Memberchage(){
+    window.open('MemberChage.html', '네이버팝업', 'width=500px,height=600px,scrollbars=yes');
+}
+
+/*test*/
+function Memberchage(){
+    window.open('MemberChage.html', '네이버팝업', 'width=500px,height=600px,scrollbars=yes');
+}
+/*관리자 수정 model*/
+$('#loginBtn').click(function () {
+    $('.loginModal').css('display', 'block');
+});
