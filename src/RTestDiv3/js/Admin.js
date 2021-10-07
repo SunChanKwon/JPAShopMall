764d84cd3bc240783669c3c2d52e8fcd2f952f26
D-Kwon <ksc7662@naver.com>
2021-10-07 20:53:18 +0900

    commit New 새로운추가

---
+++ src/RTestDiv3/js/Admin.js
@@ -0,0 +1,11 @@
+﻿$('.joinBtn').click(function () {
+    $('.AdminInsertModal').css('display', 'block');
+});
+
+$('.modalOverlay').click(function () {
+    $('.AdminInsertModal').css('display', 'none');
+});
+
+$('#joinCancelBtn').click(function () {
+    $('.AdminInsertModal').css('display', 'none');
+});
